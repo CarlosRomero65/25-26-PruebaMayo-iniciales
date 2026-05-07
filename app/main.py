5923ac8f9f515b464a82fb994bd0dcffdd95475b
Carlos Romero <carlos.romeroganfornina@gmail.com>
2026-05-07 18:08:43 +0200

    modificacion para activar el Action 3

--- app/main.py
+++ app/main.py
@@ -11,4 +11,4 @@ app.include_router(items.router)
 # Código que se añade
 @app.get("/status")
 def version():
-    return {"status": "Romero Ganfornina, Carlos - v.2"}
+    return {"status": "Romero Ganfornina, Carlos - v.3"}
